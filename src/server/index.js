01741b66429837f1ea21c232706dad12b4554eb6
Abhicoding <shinganeabhishek@gmail.com>
2018-04-17 12:41:38 +0530

    initialised express server

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+// import { port } from '_debugger'
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+app.listen(3000)

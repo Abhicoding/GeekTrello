da5d0c30db2f3ffd9e9ded7893fbab43da9495f3
Abhicoding <shinganeabhishek@gmail.com>
2018-04-18 12:21:15 +0530

    Initialized board component

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
+import Board from './Board.jsx'
+
 export default class App extends Component {
   render () {
-    return <h1>Goodbye Hello World</h1>
+    return <div>
+      <h1>Goodbye Hello World</h1>
+      <Board />
+    </div>
   }
 }
---
+++ src/client/components/Board.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import style from './../scss/board.scss'
+
+export default class Board extends Component {
+  render () {
+    return <div className='card'>
+      <span>Card Title</span>
+    </div>
+  }
+}

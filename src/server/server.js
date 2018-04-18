ef0843697ce771589a40727c1d56d54e2364cb22
Abhicoding <shinganeabhishek@gmail.com>
2018-04-18 11:09:49 +0530

    React Hello World running on express

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, './../../')))
 
 app.get('/', function (req, res) {
   res.send('hello world')

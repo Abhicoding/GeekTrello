baa4c1e4de5b99d4388200c50751a98f02c1bfe3
Abhicoding <shinganeabhishek@gmail.com>
2018-04-23 10:42:47 +0530

    Improved Board layout and prevent empty boards

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -4,15 +4,30 @@ import Boardcreate from './Boardcreate.jsx'
 import Board from './Board.jsx'
 
 export default class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      list: ['first', 'second', 'third', 'fourth']
+    }
+  }
+
+  newBoard (name) {
+    this.setState({
+      list: this.state.list.concat([name])
+    })
+  }
+
   render () {
-    const list = ['first', 'second', 'third', 'fourth']
-    const listItem = list.map((d, i) => {
+    console.log(this.state.list)
+    const listItem = this.state.list.map((d, i) => {
       return <li key={i}><Board board={d} /></li>
     })
     return <div>
       <h1>GeekTrello</h1>
-      <Boardcreate />
-      <ul>{listItem}</ul>
+      <ul>
+        <Boardcreate create={this.newBoard.bind(this)} />
+        {listItem}
+      </ul>
     </div>
   }
 }
--- src/client/components/Boardcreate.jsx
+++ src/client/components/Boardcreate.jsx
@@ -3,9 +3,20 @@ import React, { Component } from 'react'
 import './../scss/boardcreate.scss'
 
 export default class Boardcreate extends Component {
+  constructor (props) {
+    super()
+  }
+
+  passBoardName () {
+    let name = prompt('Add a name for your board')
+    if (name) {
+      this.props.create(name)
+    }
+  }
+
   render () {
     return <div className='boardcreate'>
-      <button className='cardcreate'>
+      <button className='cardcreate' onClick={this.passBoardName.bind(this)}>
         Create Board
       </button>
     </div>

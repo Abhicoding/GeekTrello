05e2a526670d3b8f763f84ac91ba272f7ef32271
Abhicoding <shinganeabhishek@gmail.com>
2018-06-21 08:51:16 +0530

    changed to server side rendering

--- src/server/index.js
+++ src/server/index.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const ReactDOMServer = require('react-dom/server')
+const mongoose = require('mongoose')
+
 const path = require('path')
+const fs = require('fs')
+
+const App = require('../client/index.js')
 
 const app = express()
-const mongoose = require('mongoose')
 
 mongoose.connect('mongodb+srv://adminGeekTrello:' + process.env.MONGO_ATLAS_PW + '@cluster0-giwk6.mongodb.net/test?retryWrites=true')
 
 const testRoutes = require('./routes/test')
 
-app.use(express.static(path.join(__dirname, './../../')))
+app.use('/build', express.static(path.join(__dirname, './../../build')))
 app.use('/test', testRoutes)
 
-app.all('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '../../index.html'))
+app.get('/', function (req, res) {
+  let app = ReactDOMServer.renderToString(<App />)
 })
 
 app.listen(5000, function () {
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const autoprefixer = require('autoprefixer')
+const nodeExternals = require('webpack-node-externals')
 
-const APP_DIR = path.resolve(__dirname, '../src/client')
-const BUILD_DIR = path.resolve(__dirname, '../build/')
+const APP_DIR = path.resolve(__dirname, '../src')
+const BUILD_DIR = path.resolve(__dirname, '../build')
 
-const config = {
+const clientConfig = {
   entry: {
-    main: APP_DIR + '/index.js'
+    main: APP_DIR + '/client/index.js'
   },
   output: {
     filename: 'bundle.js',
@@ -14,14 +17,31 @@ const config = {
   module: {
     rules: [
       {
-        test: /(\.css|.scss)$/,
-        use: [{
-          loader: 'style-loader' // creates style nodes from JS strings
-        }, {
-          loader: 'css-loader' // translates CSS into CommonJS
-        }, {
-          loader: 'sass-loader' // compiles Sass to CSS
-        }]
+        test: [/\.svg$/, /\.jpe?g$/, /\.png$/],
+        loader: 'file-loader',
+        options: {
+          name: 'build/media/[name].[ext]',
+          publicPath: url => url.replace(/public/, '')
+        }
+      },
+      {
+        test: /(\.s?css)$/,
+        use: ExtractTextPlugin({
+          use: [
+            {
+              loader: 'style-loader' // creates style nodes from JS strings
+            }, {
+              loader: 'css-loader', // translates CSS into CommonJS
+              options: {importLoaders: 1}
+            }, {
+              loader: 'sass-loader' // compiles Sass to CSS
+            },
+            {
+              loader: 'postcss-loader',
+              options: {plugins: [autoprefixer()]}
+            }
+          ]
+        })
       },
       {
         test: /\.(jsx|js)?$/,
@@ -29,12 +49,56 @@ const config = {
           loader: 'babel-loader',
           options: {
             cacheDirectory: true,
-            presets: ['react', 'es2015', 'stage-2'] // Transpiles JSX and ES6
+            presets: ['react', 'es2015'] // Transpiles JSX and ES6
           }
         }]
       }
     ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'build/css/[name].css'
+    })
+  ]
+}
+
+const serverConfig = {
+  entry: APP_DIR + '/server/index.js',
+  target: 'node',
+  node: {
+    __dirname: false
+  },
+  externals: [nodeExternals()],
+  output: {
+    filename: 'server.js',
+    path: BUILD_DIR,
+    libraryTarget: 'commonjs2'
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [
+      {
+        test: [/\.svg$/, /\.jpe?g$/, /\.png$/],
+        loader: 'file-loader',
+        options: {
+          name: 'build/media/[name].[ext]',
+          publicPath: url => url.replace(/public/, ''),
+          emit: false
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [{
+          loader: 'css-loader/locals'
+        }]
+      },
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {presets: ['react-app']}
+      }]
   }
 }
 
-module.exports = config
+module.exports = [clientConfig, serverConfig]
